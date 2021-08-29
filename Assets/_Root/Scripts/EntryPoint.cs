9633f3c2ccc8ff96888322dd29f5704832bf924e
Semyon Kachur <semyonkachur@gmail.com>
2021-08-29 22:40:52 +0500

    UnityAnalytics and UnityAds added

--- Assets/_Root/Scripts/EntryPoint.cs
+++ Assets/_Root/Scripts/EntryPoint.cs
@@ -1,5 +1,6 @@
 using Configs;
 using Profile;
+using Services.Analytics;
 using Tool;
 using UnityEngine;
 
@@ -10,12 +11,14 @@ internal class EntryPoint : MonoBehaviour
     private readonly ResourcePath _resourcePath = new ResourcePath("ScriptableObjects/ProfilePlayer");
     private PlayerStatsConfig _playerStats;
     private MainController _mainController;
+    [SerializeField] AnalyticsManager _analytics;
 
     private void Awake()
     {
         _playerStats = ResourcesLoader.LoadPlayerStats(_resourcePath);
         var profilePlayer = new ProfilePlayer(_playerStats.Speed,_playerStats.GameState,_playerStats.Transport);
         _mainController = new MainController(_placeForUi, profilePlayer);
+        _analytics.SendMainMenuOpened();
     }
 
     protected void OnDestroy()
